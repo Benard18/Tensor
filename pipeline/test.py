6807b8cab36efe5c81c05781f42983e42e23fedc
Ben Karanja <benkaranja43@gmail.com>
2018-11-30 09:32:52 +0300

    Learning on how to train models using tensorflow pipeline apis

---
+++ pipeline/test.py
@@ -0,0 +1,20 @@
+import numpy as np
+import tensorflow as tf
+
+x_input = np.random.sample((1,2))
+
+
+# using a placeholder
+x = tf.placeholder(tf.float32, shape=[1,2], name= 'X')
+
+# Define the dataset method
+dataset = tf.data.Dataset.from_tensor_slices(x)
+
+#Creating a pipeline
+iterator = dataset.make_initializable_iterator()
+get_next = iterator.get_next()
+
+with tf.Session() as sess:
+	# feed the placeholder with data
+	sess.run(iterator.initializer, feed_dict={ x: x_input })
+	print(sess.run(get_next)) 
