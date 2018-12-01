5a8c0ca60daa168e2594a7df7838246bf7e7d7e9
Ben Karanja <benkaranja43@gmail.com>
2018-12-01 12:21:57 +0300

    Strating on operators and sessions to the cause of tensorflow

---
+++ testing/operators.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+
+## Creating Operators
+'''
+The list of operators in tensorflow
+tf.add(a,b)
+tf.subtract(a,b)
+tf.multiply(a,b)
+tf.div(a,b)
+tf.pow(a,b)
+tf.exp(a)
+tf.sqrt(a,b)
+'''
+x = tf.constant([2.0], dtype= tf.float32)
+print(tf.sqrt(x))
+
+# Adding
+tensor_a = tf.constant([[1,2]],dtype=tf.int32)
+tensor_b = tf.constant([[3,4]],dtype= tf.int32)
+
+tensor_add = tf.add(tensor_a,tensor_b)
+print(tensor_add)
+
+with tf.Session() as sess:
+	print(sess.run(tensor_add))
